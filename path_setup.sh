4d92a8e2934c25d55c964ceb392c265926139c5b
Michael R. Wolf <MichaelRWolf@att.net>
2023-11-30 15:36:19 -0500

    * e Add to PATH relative to git repo toplevel

--- path_setup.sh
+++ path_setup.sh
@@ -1,1 +1,11 @@
-export PATH="$PWD/utils/:$PATH"
+#! /bin/bash
+
+GIT_REPO_BASE=$(git rev-parse --show-toplevel)
+
+# Check if the command succeeded before proceeding
+if [ $? -eq 0 ]; then
+    export PATH="${PATH}:${GIT_REPO_BASE}/utils"
+else
+    echo "Error: Not inside a Git repository." >&2
+    exit 2
+fi
