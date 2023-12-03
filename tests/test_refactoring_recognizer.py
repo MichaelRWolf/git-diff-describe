f784895789ef2179598961db46587fbb59a41014
Michael R. Wolf <MichaelRWolf@att.net>
2023-12-03 18:21:35 -0500

    ! r Move variable to class - refactoring_task_description

--- tests/test_refactoring_recognizer.py
+++ tests/test_refactoring_recognizer.py
@@ -3,6 +3,20 @@ from approvaltests import verify
 # Import your RefactoringRecognizer class. Update the path as appropriate
 from refactoring_recognizer import RefactoringRecognizer
 
+refactoring_task_description = """
+Task: infer_refactoring
+Please analyze the following unified diff output to determine if any standard refactorings can be identified. 
+If a refactoring is detected, name the refactoring and provide the relevant details, such as the functions or variables involved.
+
+Do not provide explanations for refactoring names (e.g. rename variable, extract method).  
+Assume theat the user will understand the names.  
+But _do_ provide details about the refactoring like original identifier and new identifier.
+
+Provide analysis in JSON format with fields like:
+ - refactoring-name
+ - variable-mame-original 
+ - variable-name-new
+"""
 
 def test_recognize_rename_method():
     recognizer = RefactoringRecognizer()
@@ -13,17 +27,6 @@ def test_recognize_rename_method():
     -def some_function():
     +def some_renamed_function():
          pass
-    """
-    refactoring_task_description = """
-Task: infer_refactoring
-Please analyze the following unified diff output to determine if any standard refactorings can be identified. 
-If a refactoring is detected, name the refactoring and provide the relevant details, such as the functions or variables involved.
-
-Do not provide explanations for refactoring names (e.g. rename variable, extract method).  
-Assume theat the user will understand the names.  
-But _do_ provide details about the refactoring like original identifier and new identifier.
-
-Provide analysis in JSON format with fields like 'refactoring name', 'variable mame - original', 'variable name - new'.
     """
 
     recognizer.add_task(refactoring_task_description)
