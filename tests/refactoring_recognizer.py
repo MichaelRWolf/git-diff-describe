8a19c3b27b3adf85cdc5f5783fbe9f13d2f06218
Michael R. Wolf <MichaelRWolf@att.net>
2024-01-08 16:55:46 -0500

    ! f Made the class be a script

--- tests/refactoring_recognizer.py
+++ tests/refactoring_recognizer.py
@@ -1,4 +1,84 @@
+#!/usr/bin/env python3
+
 import subprocess
+import sys
+
+refactoring_task_description = """
+# Task: infer_refactoring
+
+Analyze the following output provided in 'unified diff' format
+Determine if any standard software code refactorings can be identified. 
+If a refactoring is detected, provide output including
+  - preferred refactoring name
+  - attributes specific to this refactoring
+
+Assume that the user will understand common refactorings, so do not provide explanations for them.
+
+# Preferred refactoring name(s)
+Many similar names are commonly used for a refactoring.  
+When you report a refactoring, use 'preferred name' below instead of one of the 'alternate names'.
+
+Further describe the refactoring by providing the attributes listed for each refactoring.
+
+Provide additional attributes if you think they are necessary.
+
+- preferred_name: Rename Variable
+  alternate_names:
+    - Rename Identifier
+  attributes:
+    - original_name
+    - new_name
+
+- preferred_name: Rename Function
+  alternate_names:
+    - Rename Method
+  attributes:
+    - original_name
+    - new_name
+
+- preferred_name: Extract Method
+  alternate_names:
+    - Extract Function
+    - Extract Procedure
+  attributes:
+    - new_name
+
+- preferred_name: Move Method
+  alternate_names:
+    - Move Function
+    - Move Procedure
+
+- preferred_name: Extract Class
+  alternate_names:
+    - Extract Module
+
+- preferred_name: Change Signature
+  alternate_names:
+    - Modify Function Signature
+
+- preferred_name: Replace Conditional with Polymorphism
+  alternate_names:
+    - Replace Conditional with Inheritance
+
+# Format  
+Generate YAML output for refactoring actions. 
+Ensure that string values are enclosed in double quotes only when necessary. 
+Avoid adding optional quotes.
+
+Example:
+- refactoring_name: Rename Variable
+  original_name: vol
+  new_name: volume
+
+
+For each refactoring, output these fields.
+ - refactoring_name - Use the preferred name, not alternate names
+ - [attributes] - See below for suggested attributes based on 'refactoring-name'.
+     Do not output 'attributes' as a collection for attributes.  
+     Simply place the attributes at the same level as 'refactoring_name'.
+ - [other] - if you feel that other information would be necessary to categorize the refactoring
+"""
+
 
 class RefactoringRecognizer:
     def __init__(self):
@@ -47,5 +127,26 @@ class RefactoringRecognizer:
 
     def analysis(self):
         return self.gpt_result.stdout
+
     def assemble_prompt(self):
         return self.__str__()
+
+
+def main():
+    git_diff_u_r_string = sys.stdin.read()
+    print(git_diff_u_r_string)
+    print("Goodbye")
+
+    recognizer = RefactoringRecognizer()
+    recognizer.add_task(refactoring_task_description)
+    recognizer.add_diff(git_diff_u_r_string)
+
+    recognizer.chatGPT_prompt_and_return()
+
+    print(recognizer.subprocess_info())
+
+    print(recognizer.analysis())
+
+
+if __name__ == "__main__":
+    main()
