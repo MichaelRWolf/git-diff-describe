5476a8b37d635aa7fb5d48d364afe060c422206f
Michael R. Wolf <MichaelRWolf@att.net>
2024-01-09 15:35:07 -0500

    - r Improved prompt.  Raw YAML (not code block), and 'other changes'

--- src/refactoring_recognizer.py
+++ src/refactoring_recognizer.py
@@ -7,7 +7,7 @@ refactoring_task_description = """
 # Task: infer_refactoring
 
 Analyze the following output provided in 'unified diff' format
-Determine if any standard software code refactorings can be identified. 
+Determine if any standard software code refactorings can be identified.
 If a refactoring is detected, provide output including
   - preferred refactoring name
   - attributes specific to this refactoring
@@ -15,7 +15,7 @@ If a refactoring is detected, provide output including
 Assume that the user will understand common refactorings, so do not provide explanations for them.
 
 # Preferred refactoring name(s)
-Many similar names are commonly used for a refactoring.  
+Many similar names are commonly used for a refactoring.
 When you report a refactoring, use 'preferred name' below instead of one of the 'alternate names'.
 
 Further describe the refactoring by providing the attributes listed for each refactoring.
@@ -60,24 +60,53 @@ Provide additional attributes if you think they are necessary.
   alternate_names:
     - Replace Conditional with Inheritance
 
-# Format  
-Generate YAML output for refactoring actions. 
-Ensure that string values are enclosed in double quotes only when necessary. 
+# Format
+Generate YAML output for refactoring actions.
+Output the YAML directly.
+ - Do not preface the YAML with the string literal "yaml".
+ - Do not display it in a text box.
+ - Do not surround it with multi-line quotes.
+Ensure that string values are enclosed in double quotes only when necessary.
+Quotes are optional around values that contain only letters, numbers, and underscores.
 Avoid adding optional quotes.
+Do not add quotes around values that contain only letters, numbers, and underscores.
+Ensure that there are no optional blank lines between elements in the YAML output.
+
+In the following example, notice that these values are not surrounded by quotes
+ - c
+ - city
+ - s
+ - state
+ - z
+ - zip_code
+
+
+Also notice that there is not a blank line separating the items with new name: city, state, zip_code.
+These items are adjacent with no separation.
 
 Example:
 - refactoring_name: Rename Variable
-  original_name: vol
-  new_name: volume
+  original_name: c
+  new_name: city
+- refactoring_name: Rename Variable
+  original_name: s
+  new_name: state
+- refactoring_name: Rename Variable
+  original_name: z
+  new_name: zip_code
 
 
 For each refactoring, output these fields.
  - refactoring_name - Use the preferred name, not alternate names
  - [attributes] - See below for suggested attributes based on 'refactoring-name'.
-     Do not output 'attributes' as a collection for attributes.  
+     Do not output 'attributes' as a collection for attributes.
      Simply place the attributes at the same level as 'refactoring_name'.
  - [other] - if you feel that other information would be necessary to categorize the refactoring
  
+If you notice changes that are not refactorings, please describe the changes in the output structure with key 'other changes'.
+
+Do NOT return YAML in a code block.
+
 If you notice changes that are not refactorings, please describe the changes in the output structure with key 'other changes'.
 
 Do NOT return YAML in a code block.
@@ -86,7 +115,7 @@ Do NOT return YAML in a code block.
 
 class RefactoringRecognizer:
     def __init__(self):
-        self.task = None
+        self.task = refactoring_task_description
         self.gpt_result = None
         self.diff_output = None
 
