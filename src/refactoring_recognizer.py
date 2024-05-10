11964c3d31bd455e0308bc1849269cfda77d0b55
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-10 14:17:13 -0400

    Commenting out debugging print statements
    The diff output provided indicates that print statements in the Python file `refactoring_recognizer.py` have been commented out; no other significant changes are visible in the diff provided. This change doesn't map directly to any standard refactoring patterns; instead, it's more of a modification to the output/logging behavior of the program. Here's the analysis based on the provided task requirements:
    
    ```yaml
    - other changes: commenting out print statements
    ```

--- src/refactoring_recognizer.py
+++ src/refactoring_recognizer.py
@@ -223,10 +223,10 @@ def fetch_openai_api_key():
 
 def main():
     git_diff_u_r_string = sys.stdin.read()
-    print("Sending this git-diff to be analyzed..." +
-          "---------------------------------------" +
-          git_diff_u_r_string,
-          file=sys.stdout)
+    # print("Sending this git-diff to be analyzed..." +
+    #       "---------------------------------------" +
+    #       git_diff_u_r_string,
+    #       file=sys.stdout)
 
     recognizer = RefactoringRecognizer()
     recognizer.add_task(refactoring_task_description)
@@ -234,7 +234,7 @@ def main():
 
     recognizer.chatgpt_prompt_and_return()
 
-    print(recognizer.subprocess_info(), file=sys.stdout)
+    # print(recognizer.subprocess_info(), file=sys.stdout) 
     print(recognizer.analysis())
 
 
