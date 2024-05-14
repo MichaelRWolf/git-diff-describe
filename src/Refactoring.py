cf9eb7945ce6d555abc7b165f7102e80452bb317
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-13 22:38:07 -0400

    Separate 'Rename *' from other refactoring names

--- src/Refactoring.py
+++ src/Refactoring.py
@@ -6,8 +6,11 @@ class Refactoring:
     def __str__(self):
         name = self.name
 
-        original_name = self.attributes["original_name"]
-        new_name = self.attributes["new_name"]
-        description = f"{name} to {new_name} from {original_name}"
-
+        match name:
+            case "Rename Variable" | "Rename Method" | "Rename Class":
+                original_name = self.attributes["original_name"]
+                new_name = self.attributes["new_name"]
+                description = f"{name} to {new_name} from {original_name}"
+            case _ :
+                description = f"{name}"
         return description
