62a81d61f7f125506e0a0513d51866fac7b7a84b
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-13 22:04:13 -0400

    New test: test_rename_refactorings

--- tests/test_Refactoring.py
+++ tests/test_Refactoring.py
@@ -1,3 +1,4 @@
+from shlex import join
 from unittest import TestCase
 
 from approvaltests import verify
@@ -13,7 +14,23 @@ class TestRefactoring(TestCase):
 
     def test_implicit_stringify_displays_name_and_attributes(self):
         attributes = {'original_name': 'fn',
-                      'new_name'     : 'file_name'}
+                      'new_name': 'file_name'}
         obj = Refactoring("Rename Variable", attributes)
         value = str(obj)
+        verify(value)
+
+    def test_rename_refactorings(self):
+        rename_variable = Refactoring("Rename Variable",
+                                      {"original_name": "maxSpeed",
+                                       "new_name": "speedMax"});
+
+        rename_method = Refactoring("Rename Method",
+                                      {"original_name": "m1",
+                                       "new_name": "mach1"});
+        rename_class = Refactoring("Rename Class",
+                                      {"original_name": "FastCar",
+                                       "new_name": "WayOutOfHere"});
+
+
+        value = ''.join( [str(rename_variable)+"\n", str(rename_method)+"\n", str(rename_class)+"\n"])
         verify(value)
