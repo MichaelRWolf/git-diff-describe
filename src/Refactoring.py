e4e43f354c94f44d8e14d08fdc397c9568dd5a76
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-13 21:29:40 -0400

    Improve stringify for 'Extract Variable'
    sha256:5a877809f766c62cd479119ab5ae319501214213f9e6454f79bf13a19d115b4e
    - refactoring_name: Rename Variable
      original_name: fn
      new_name: file_name
    - refactoring_name: Rename Variable
      original_name: original_name
      new_name: name
    - refactoring_name: Rename Variable
      original_name: name
      new_name: description

--- src/Refactoring.py
+++ src/Refactoring.py
@@ -1,6 +1,13 @@
 class Refactoring:
-    def __init__(self, name):
+    def __init__(self, name, attributes):
+        self.attributes = attributes
         self.name = name
 
     def __str__(self):
-        return self.name
+        name = self.name
+
+        original_name = self.attributes["original_name"]
+        new_name = self.attributes["new_name"]
+        description = f"{name} to {new_name} from {original_name}"
+
+        return description
--- tests/test_Refactoring.py
+++ tests/test_Refactoring.py
@@ -8,12 +8,13 @@ from Refactoring import Refactoring
 
 class TestRefactoring(TestCase):
     def test_harnesses_are_installed(self):
-        obj = Refactoring("xx")
+        obj = Refactoring("xx", attributes)
         assert True
 
     def test_implicit_stringify_displays_name(self):
         attributes = {'original_name': 'fn',
                       'new_name'     : 'file_name'}
-        obj = Refactoring("Rename Variable")
-        value = f"{obj}\n    {attributes}"
+        obj = Refactoring("Rename Variable", attributes)
+        # value = f"{obj}\n    {attributes}"
+        value = str(obj)
         verify(value)
