3abd706163de412f71e9ffebf032acc640c051c4
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-24 18:23:35 -0400

    Enhanced readability for 'Extract Class' formatting
    Extract Class: new_class (from original_class)
        {'refactoring_name': 'Extract Class', 'original_class': 'original_class', 'new_class': 'new_class', 'classes_in_hierarchy': []}
    Non-refactoring:  {'change': 'Enhance readability with default values for attributes'}
    Non-refactoring:  {'change': 'Add comments for clarity'}
    Non-refactoring:  {'change': 'Improve multi-line string formatting'}
    
    **YAML (probably)**
    
    - refactoring_name: Extract Class
      original_class: original_class
      new_class: new_class
      classes_in_hierarchy: []
    - change: "Enhance readability with default values for attributes"
    - change: "Add comments for clarity"
    - change: "Improve multi-line string formatting"

--- src/Refactoring.py
+++ src/Refactoring.py
@@ -17,15 +17,19 @@ class Refactoring:
                 description = f"{name}: {new_name}"
 
             case "Extract Class" | "Replace Conditional with Polymorphism":
-                new_name = self.attributes.get("new_class", "<NewClass>")
-                original_name = self.attributes.get("original_class")
-                classes_in_hierarcy = self.attributes.get("classes_in_hierarchy")
+                # Required attributes
+                refactoring_name = self.attributes.get("refactoring_name")
+                original_class = self.attributes.get("original_class")
+
+                # Optional attributes
+                new_class = self.attributes.get("new_class", "<NewClass>")
+                classes_in_hierarcy = self.attributes.get("classes_in_hierarchy", [])
+
                 description = (
-                        f"{name}: {new_name}"
-                        + (f" (from {original_name})" if original_name else "")
-                        + (f" classes_in_hierarchy: {classes_in_hierarcy})" if classes_in_hierarcy else "")
-                        + "\n"
-                        + f"    {self.attributes}")
+                    f"{refactoring_name}: {new_class}"
+                    + (f" (from {original_class})" if original_class else "")
+                    + (f" classes_in_hierarchy: {classes_in_hierarcy})" if classes_in_hierarcy else "")
+                    + ("\n" + f"    {self.attributes}") if self.attributes else "")
 
             case _:
                 description = (f"{name} - Unhandled Refactoring\n"
