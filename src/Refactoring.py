3e7e0cef021d378fe534a44d7993e549be6d5774
Michael R. Wolf <MichaelRWolf@att.net>
2024-05-24 18:46:44 -0400

    Introduce variable: `self_attributes_copy` which can be mutated to find unanticipated attributes
    Rename Variable: self_attributes (from attributes)
    Non-refactoring:  {'change': 'improve readability by introducing a temporary variable self_attributes_copy for modified dictionary'}
    Non-refactoring:  {'change': 'add missing colon in the f-string'}
    Non-refactoring:  {'change': 'update string formatting for better readability'}
    
    **YAML (probably)**
    
    - refactoring_name: Rename Variable
      original_name: attributes
      new_name: self_attributes
    - change: "improve readability by introducing a temporary variable self_attributes_copy for modified dictionary"
    - change: "add missing colon in the f-string"
    - change: "update string formatting for better readability"

--- src/Refactoring.py
+++ src/Refactoring.py
@@ -6,33 +6,35 @@ class Refactoring:
     def __str__(self):
         name = self.name
 
+        self_attributes = self.attributes
         match name:
             case "Rename Variable" | "Rename Method" | "Rename Function" | "Rename Class":
-                original_name = self.attributes["original_name"]
-                new_name = self.attributes["new_name"]
+                original_name = self_attributes["original_name"]
+                new_name = self_attributes["new_name"]
                 description = f"{name}: {new_name} (from {original_name})"
 
             case "Extract Variable" | "Extract Function" | "Extract Method":
-                new_name = self.attributes.get("new_name")
+                new_name = self_attributes.get("new_name")
                 description = f"{name}: {new_name}"
 
             case "Extract Class" | "Replace Conditional with Polymorphism":
-                # Required attributes
-                refactoring_name = self.attributes.get("refactoring_name")
-                original_class = self.attributes.get("original_class")
-
-                # Optional attributes
-                new_class = self.attributes.get("new_class", "<NewClass>")
-                classes_in_hierarcy = self.attributes.get("classes_in_hierarchy", [])
+                self_attributes_copy = self_attributes.copy()
+                refactoring_name = self_attributes_copy.pop("refactoring_name")
+                original_class = self_attributes_copy.pop("original_class", "<NoOriginalClass>")
+                new_class = self_attributes_copy.pop("new_class", "<NewClass>")
+                classes_in_hierarcy = self_attributes_copy.pop("classes_in_hierarchy", [])
 
                 description = (
-                    f"{refactoring_name}: {new_class}"
-                    + (f" (from {original_class})" if original_class else "")
-                    + (f" classes_in_hierarchy: {classes_in_hierarcy})" if classes_in_hierarcy else "")
-                    + ("\n" + f"    {self.attributes}") if self.attributes else "")
+                        f"{refactoring_name}: {new_class}"
+                        + (f" (from {original_class})" if original_class else "")
+                        + (f" classes_in_hierarchy: {classes_in_hierarcy})" if classes_in_hierarcy else "")
+                        + (
+                            ("\n" + f"    {self_attributes_copy}") if self_attributes_copy else ""
+                        ) # Extra parens needed.  Don't know why.
+                )
 
             case _:
                 description = (f"{name} - Unhandled Refactoring\n"
-                               f"    {self.attributes}")
+                               f"    {self_attributes}")
 
         return description
